71ddfaed8f39e72a13f6fc96821633908900aa74
DFLotus <deondraefrancis101@gmail.com>
2023-10-21 01:39:57 -0400

    SM-16: Connected to supabase postsql database

--- server/models/index.js
+++ server/models/index.js
@@ -6,14 +6,20 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env]; //const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  //sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.development.database, 
+    config.development.username,
+    config.development.password,
+    config.development
+  );
 }
 
 fs
