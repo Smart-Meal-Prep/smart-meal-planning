eac95230a968fcd4cce13d806efdb20158f9f4c4
matteoSaputo <matteosaputo1201@gmail.com>
2023-11-05 14:17:58 -0500

    SM-30: Updated profile tests

--- server/test/profile.test.js
+++ server/test/profile.test.js
@@ -1,3 +1,4 @@
+const { json } = require('sequelize');
 const { getProfile } = require('../controllers/Profile');
 const { Profile, User } = require('../models');
 
@@ -19,8 +20,8 @@ const res = {
     json: jest.fn((x) => x)
 };
 
-describe('getProfile', () => {
-    it("should return user profile on valid userid", async () => {
+describe('On valid get profile', () => {
+    it('should return status code of 200 if user profile was succesfully retrieved', async () => {
         const req = {
             body : {
                 UserId: 1
@@ -29,14 +30,14 @@ describe('getProfile', () => {
 
         const user = {
             id: 1,
-            username: "John",
-            password: "1234",
-            email: "john@test.com"
+            username: 'John',
+            password: '1234',
+            email: 'john@test.com'
         }
         const profile = {
             id: 1,
-            allergies: ["Peanuts"],
-            preferences: ["Vegetarian"],
+            allergies: ['Peanuts'],
+            preferences: ['Vegetarian'],
             UserId: 1 //same id as user from above
         }
 
@@ -49,4 +50,32 @@ describe('getProfile', () => {
         expect(res.json).toHaveBeenCalledWith(profile);
 
     })
+    
+    it('should return status code of 400 when UserId if missing', async () => {
+        const req = {
+            body: {}
+        };
+
+        await getProfile(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({
+            error: 'Error empty userid'
+        })
+    })
+    
+    it('should return error and status code 400 when User.findOne or Profile.findOne throws exception', async () => {
+        const req = {
+            body: {
+                UserId: 1
+            }
+        }
+
+        Profile.findOne.mockRejectedValue(new Error('Database Error'));
+        await getProfile(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({error: 'Error retrieving user profile'})
+    })
+
 })
