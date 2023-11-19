d1f0541145a562f082a08b6a96707e40775a6680
DFLotus <deondraefrancis101@gmail.com>
2023-11-19 01:16:23 -0500

    SM-51: Added spacing to navbar

--- front-end/src/component/Dashboard/TopBar.js
+++ front-end/src/component/Dashboard/TopBar.js
@@ -1,18 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import '../../styles/TopBar.css'
 
 const TopBar = () => {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#">Navbar</Navbar.Brand>
+            <Navbar.Brand href="#" className='nav-brand'>Navbar</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarNavDropdown" />
             <Navbar.Collapse id="navbarNavDropdown">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/inventory">Inventory</Nav.Link>
-                    <Nav.Link href="#">Calendar</Nav.Link>
-                    <Nav.Link href="#">Recipes</Nav.Link>
-                    <Nav.Link href="/login">Sign In</Nav.Link>
-                    <NavDropdown title="Profile" id="navbarDropdownMenuLink">
+                <Nav className="mr-auto" >
+                    <Nav.Link className='nav-item' href="/inventory">Inventory</Nav.Link>
+                    <Nav.Link className='nav-item' href="#">Calendar</Nav.Link>
+                    <Nav.Link className='nav-item' href="#">Recipes</Nav.Link>
+                    <Nav.Link className='nav-item' href="/login">Sign In</Nav.Link>
+                    <NavDropdown className='nav-item' title="Profile" id="navbarDropdownMenuLink">
                         <NavDropdown.Item href="/profile">View settings</NavDropdown.Item>
                         <NavDropdown.Item href="#">Sign Out</NavDropdown.Item>
                     </NavDropdown>
