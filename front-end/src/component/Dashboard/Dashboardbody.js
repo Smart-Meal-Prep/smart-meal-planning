099197ccd9b5e4dcadacfe7a0e3a4b228b3171de
DFLotus <deondraefrancis101@gmail.com>
2023-11-24 14:57:39 -0500

    SM-51: Added logo to body

--- front-end/src/component/Dashboard/Dashboardbody.js
+++ front-end/src/component/Dashboard/Dashboardbody.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Image } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 import cooking_logo from "../../assets/cooking_logo.png"
 import food_background from "../../assets/foodpat.png"
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,8 +9,11 @@ const Dashboardbody = () => {
     return (
         <div className="img-background">
             <Image src={food_background} fluid className="img-area" />
+            <div className="logo-container">
+                <Image src={cooking_logo} fluid className="name-logo" />
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Dashboardbody
