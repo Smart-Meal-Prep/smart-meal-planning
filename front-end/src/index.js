19e5a7e9341e6121f7c1b2c17a0c322cb22bddf7
DFLotus <deondraefrancis101@gmail.com>
2023-12-05 22:34:12 -0500

    SM-62: Added router dom prop

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -7,7 +7,7 @@ import './styles/index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
