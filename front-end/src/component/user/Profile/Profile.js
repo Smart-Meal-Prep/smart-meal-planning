940a5c33cae0c5c9af71560e00e946d231085df3
DFLotus <deondraefrancis101@gmail.com>
2023-12-12 00:41:28 -0500

    SM-67: Profile page progress

--- front-end/src/component/user/Profile/Profile.js
+++ front-end/src/component/user/Profile/Profile.js
@@ -3,6 +3,10 @@ import endPoints from '../../../config/fetch'
 import lists from '../../../config/list';
 import Select from 'react-select';
 import UserInfo from '../../../config/UserInfo';
+import NavigationBar from '../../NavigationBar';
+import DashboardFooter from '../../DashboardFooter';
+import '../../../styles/Profile.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Profile = (props) => {
 
@@ -232,76 +236,148 @@ const Profile = (props) => {
 
     }
 
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const renderContent = () => {
+        switch (selectedOption) {
+            case 'My Information':
+                return (
+                    <div>
+                        {/* Render content for My Information */}
+                        {/* Example: Display user information */}
+                        <h2>User Information</h2>
+                        {/* Add your content here */}
+                    </div>
+                );
+
+            case 'Update Preference':
+                return (
+                    <div>
+                        <form>
+                            Add Allergy:
+                            <Select
+                                options={lists.ingredientsOptions}
+                                onChange={(e) => setNewAllergy(e.label)}
+                                value={{ label: newAllergy }}
+                            />
+                            <button type="button" onClick={handleAddAllergy}>
+                                Add
+                            </button>
+                        </form>
+                        <form>
+                            Remove Allergy:
+                            <Select
+                                options={props.profile.allergiesOptions}
+                                onChange={(e) => setRemovingAllergy(e.label)}
+                                value={{ label: removingAllergy }}
+                            />
+                            <button type="button" onClick={handleRemoveAllergy}>
+                                Remove
+                            </button>
+                        </form>
+                        <form>
+                            Add Preference:
+                            <Select
+                                options={lists.preferencesOptions}
+                                value={{ label: newPreference }}
+                                onChange={(e) => setNewPreference(e.label)}
+                            />
+                            <button type="button" onClick={handleAddPreference}>
+                                Add
+                            </button>
+                        </form>
+
+                        <form>
+                            Remove Preference:
+                            <Select
+                                options={props.profile.preferencesOptions}
+                                value={{ label: removingPreference }}
+                                onChange={(e) => setRemovingPreference(e.label)}
+                            />
+                            <button type="button" onClick={handleRemovePreference}>
+                                Remove
+                            </button>
+                        </form>
+                        <h2>Allergies:</h2>
+                        <ul>
+                            {props.profile.allergies && props.profile.allergies.map((allergy, index) => (
+                                <li key={index}>{allergy}</li>
+                            ))}
+                        </ul>
+
+                        <h2>Preferences:</h2>
+                        <ul>
+                            {props.profile.preferences && props.profile.preferences.map((preference, index) => (
+                                <li key={index}>{preference}</li>
+                            ))}
+                        </ul>
+                    </div>
+                );
+
+            case 'Favorite Meals':
+                return (
+                    <div>
+                        {/* Render content for Favorite Meals */}
+                        {/* Example: Display favorite meals */}
+                        <h2>Favorite Meals</h2>
+                        {/* Add your content here */}
+                    </div>
+                );
+
+            default:
+                return 'My Information';
+        }
+    };
+
     return (
-        <div>
-            <h1>Profile</h1>
-
-            <div>
-                <form>
-                    Add Allergy:
-                    <Select
-                        options={lists.ingredientsOptions}
-                        onChange={(e) => setNewAllergy(e.label)}
-                        value={{ label: newAllergy }}
-                    />
-                    <button type="button" onClick={handleAddAllergy}>
-                        Add
-                    </button>
-                </form>
-
-                <form>
-                    Remove Allergy:
-                    <Select
-                        options={props.profile.allergiesOptions}
-                        onChange={(e) => setRemovingAllergy(e.label)}
-                        value={{ label: removingAllergy }}
-                    />
-                    <button type="button" onClick={handleRemoveAllergy}>
-                        Remove
-                    </button>
-                </form>
+        <div className='profile-div'>
+            <NavigationBar />
+            <div className="row">
+                {/* Sidebar */}
+                <div className="col-md-3">
+                    <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark profile-bar">
+                        <p className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                            <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap" /></svg>
+                            <span className="profile-title">Profile Settings</span>
+                        </p>
+                        <hr />
+                        <ul className="nav nav-pills flex-column mb-auto">
+                            <li className='list-section'>
+                                <button
+                                    className="btn nav-link text-white list-section"
+                                    onClick={() => setSelectedOption('My Information')}
+                                >
+                                    <svg className="bi me-2" width="16" height="16"><use xlinkHref="#speedometer2" /></svg>
+                                    My Information
+                                </button>
+                            </li>
+                            <li className='list-section'>
+                                <button
+                                    className="btn nav-link text-white list-section"
+                                    onClick={() => setSelectedOption('Update Preference')}
+                                >
+                                    <svg className="bi me-2" width="16" height="16"><use xlinkHref="#table" /></svg>
+                                    Update Preference
+                                </button>
+                            </li>
+                            <li className='list-section'>
+                                <button
+                                    className="btn nav-link text-white list-section"
+                                    onClick={() => setSelectedOption('Favorite Meals')}
+                                >
+                                    <svg className="bi me-2" width="16" height="16"><use xlinkHref="#grid" /></svg>
+                                    Favorite Meals
+                                </button>
+                            </li>
+                        </ul>
+                        <hr />
+                    </div>
+                </div>
+                <div className="col-md-9 p-3">
+                    {renderContent()}
+                </div>
             </div>
-
-            <div>
-                <form>
-                    Add Preference:
-                    <Select
-                        options={lists.preferencesOptions}
-                        value={{ label: newPreference }}
-                        onChange={(e) => setNewPreference(e.label)}
-                    />
-                    <button type="button" onClick={handleAddPreference}>
-                        Add
-                    </button>
-                </form>
-
-                <form>
-                    Remove Preference:
-                    <Select
-                        options={props.profile.preferencesOptions}
-                        value={{ label: removingPreference }}
-                        onChange={(e) => setRemovingPreference(e.label)}
-                    />
-                    <button type="button" onClick={handleRemovePreference}>
-                        Remove
-                    </button>
-                </form>
-            </div>
-
-            <h2>Allergies:</h2>
-            <ul>
-                {props.profile.allergies && props.profile.allergies.map((allergy, index) => (
-                    <li key={index}>{allergy}</li>
-                ))}
-            </ul>
-
-            <h2>Preferences:</h2>
-            <ul>
-                {props.profile.preferences && props.profile.preferences.map((preference, index) => (
-                    <li key={index}>{preference}</li>
-                ))}
-            </ul>
-
+            <DashboardFooter />
         </div>
     );
 }
